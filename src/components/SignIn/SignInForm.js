32040d00ada51089c5c7e2f51ba3578a59099eb7
JoaoVRP <joaovrp.mec.ufsc@gmail.com>
2023-03-28 01:33:42 -0300

    feat: sign in form connected to the api

--- src/components/SignIn/SignInForm.js
+++ src/components/SignIn/SignInForm.js
@@ -1,25 +1,78 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import useSignIn from '../../hooks/api/useSignIn';
 import { Button } from '../Form/Button';
+import { ErrorMessage } from '../Form/ErrorMessage';
 import { StyledForm } from '../Form/Form';
 import { Input } from '../Form/Input';
 import { Label } from '../Form/Label';
+import LoadingDots from '../Form/Loading';
 
 export default function SignInForm() {
+  const [body, setBody] = useState({
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState(false);
+
+  const { signInLoading, signIn } = useSignIn();
+  const navigate = useNavigate();
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+
+    setError(false);
+
+    setBody({ ...body, [name]: value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await signIn(body);
+
+      toast.success('Login realizado com sucesso!');
+
+      navigate('/feed');
+    } catch (err) {
+      setError(true);
+    }
+  }
+
   return (
-    <StyledForm>
+    <StyledForm onSubmit={handleSubmit}>
       <Label htmlFor="email">Email:</Label>
-      <Input id="email" placeholder="Insira seu email" required />
+      <Input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Insira seu email"
+        value={body.email}
+        onChange={handleInputChange}
+        disabled={signInLoading}
+        error={error}
+        required
+      />
 
       <Label htmlFor="password">Senha:</Label>
-      <Input type="password" id="password" placeholder="Insira sua senha" required />
+      <Input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Insira sua senha"
+        value={body.password}
+        onChange={handleInputChange}
+        disabled={signInLoading}
+        error={error}
+        required
+      />
+      {error && <ErrorMessage>Email ou senha incorretos!</ErrorMessage>}
 
-      <StyledButton type="submit" disabled={false}>
-        Entrar
-      </StyledButton>
+      <Button type="submit" disabled={signInLoading}>
+        {signInLoading ? <LoadingDots /> : 'Entrar'}
+      </Button>
     </StyledForm>
   );
 }
-
-const StyledButton = styled(Button)`
-  cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
-`;
