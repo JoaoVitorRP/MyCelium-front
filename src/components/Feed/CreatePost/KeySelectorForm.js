c1e79f468a951fc14d6747d485a38955f1b50e45
JoaoVRP <joaovrp.mec.ufsc@gmail.com>
2023-04-11 16:05:52 -0300

    style: addressed spelling mistakes and simplified keys

--- src/components/Feed/CreatePost/KeySelectorForm.js
+++ src/components/Feed/CreatePost/KeySelectorForm.js
@@ -13,46 +13,44 @@ export default function KeySelectorForm({ selectedKey, setSelectedKey }) {
   return (
     <StyledForm>
       <InputContainer>
-        <Label>Selecione a característica morfológica de maior evidência no corpo frutífero de seu fungo:</Label>
+        <Label>Selecione a característica morfológica de maior evidência nos corpos frutíferos de seu fungo:</Label>
         <CheckboxInput
-          label="Formato de chapéu com lamelas na face inferior e geralmente com estípite."
+          label="A. Formato de chapéu com lamelas na face inferior e geralmente com estípite."
           id="A"
           name="fruitbody"
           isSelected={selectedKey === 'A'}
           handleCheckboxChange={handleCheckboxChange}
         />
         <CheckboxInput
-          label="Estipitado com cabeça ou chapéu, sem lamelas na face inferior."
+          label="B. Estipitados com cabeça ou chapéu, sem lamelas na face inferior."
           id="B"
           name="fruitbody"
           isSelected={selectedKey === 'B'}
           handleCheckboxChange={handleCheckboxChange}
         />
         <CheckboxInput
-          label="Em forma de calíce, tigela, funil, petalóide ou espatulado."
+          label="C. Em forma de calíce, tigela, funil, leque a petalóides ou espatulados."
           id="C"
           name="fruitbody"
           isSelected={selectedKey === 'C'}
           handleCheckboxChange={handleCheckboxChange}
         />
         <CheckboxInput
-          label="Globoso, tuberoso, piriforme, turbinado ou estrelar."
+          label="D. Mais ou menos claviformes, cilíndricos, ramificados ou não."
           id="D"
           name="fruitbody"
           isSelected={selectedKey === 'D'}
           handleCheckboxChange={handleCheckboxChange}
         />
         <CheckboxInput
-          label="Mais ou menos claviforme, cilíndrico, ramificado ou não."
+          label="E. Formando crosta sobre a madeira, semi-aderidos ao substrato, irregularmente lobados, foliáceos ou cerebriformes."
           id="E"
           name="fruitbody"
           isSelected={selectedKey === 'E'}
           handleCheckboxChange={handleCheckboxChange}
         />
         <CheckboxInput
-          label="Em forma de leque, disposto em prateleiras, 
-            com ou sem estípite lateral ou irregularmente lobado, 
-            foliáceo, auriculado, conchado, cerebriforme ou formando crostas sabre a madeira."
+          label="F. Dispostos em prateleiras, com ou sem estípite lateral, achatados, conchados ou auriculados."
           id="F"
           name="fruitbody"
           isSelected={selectedKey === 'F'}
